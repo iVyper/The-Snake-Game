1a0db85beb65b2b2e26973067d9b18d5dbb01f10
Ivis Perdomo <iperdomo@ivyper.dev>
2021-01-08 00:00:00 -0500

    feat(scoreboard): persist and display high score
    - Load high score from high_score.txt on startup (default to 0 if missing).
    - Show both current score and high score in the on-screen display.
    - On reset, update high_score.txt when current score exceeds previous high.

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,10 @@
 """
 Scoreboard Module
 
-This module defines the Scoreboard class which handles displaying the current score on the screen.
-It also shows a game over message when the game ends.
+This module defines the Scoreboard class which tracks and displays:
+- Current score
+- High score (persisted in 'high_score.txt')
+It also resets scores when the snake collides.
 """
 
 from turtle import Turtle
@@ -10,40 +12,46 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
-
 class Scoreboard(Turtle):
     """
-    Represents the scoreboard that keeps track of the player's score.
-
-    Inherits from Turtle. It displays the current score at the top of the screen and shows a game over message when needed.
+    Displays the score and high score at the top of the screen.
+    Can reset itself when the game restarts, updating the high score file if needed.
     """
-
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.hideturtle()  # Hide the turtle shape.
-        self.color('white')  # Score text color.
-        self.penup()  # Don't draw lines when moving.
-        self.goto(0, 270)  # Position the score near the top of the screen.
-        self.show_score()  # Display the initial score.
+        # Load high score from file, defaulting to 0 if file is missing or invalid.
+        with open("high_score.txt", mode="r") as f:
+            self.high_score = int(f.read())
+        self.hideturtle()
+        self.color('white')
+        self.penup()
+        self.goto(0, 270)  # Position the text at the top center.
+        self.show_score()
 
     def increase_score(self):
         """
-        Increases the current score by one and updates the display.
+        Increment the current score by one and update the on-screen display.
         """
         self.score += 1
         self.show_score()
 
     def show_score(self):
         """
-        Clears the previous score and writes the new score on the screen.
+        Clear previous text and write the updated current score and high score.
         """
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
+    def reset(self):
         """
-        Displays the game over message in the center of the screen.
+        Reset the current score to zero. If the current score exceeds the high score,
+        update the high score in memory and write it to the file.
+        Finally, refresh the display to show Score: 0 and the (new) high score.
         """
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode="w") as f:
+                f.write(str(self.high_score))
+        self.score = 0
+        self.show_score()
