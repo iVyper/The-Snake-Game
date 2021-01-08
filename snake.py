6c6f8fff974bdc0b5d075a0e1cbe451d1aa63ac5
Ivis Perdomo <iperdomo@ivyper.dev>
2021-01-08 00:00:00 -0500

    feat(snake): add reset method to restart snake on collision
    - Implement Snake.reset() to move all segments off-screen, clear list, and
      recreate the initial snake body.
    - Reset head reference so controls and movement work after a restart.

--- snake.py
+++ snake.py
@@ -1,99 +1,105 @@
 """
 Snake Module
 
-This module defines the Snake class, which manages the snake's behavior including movement,
-growth, and direction control.
+This module defines the Snake class, which manages the snake’s segments,
+movement, growth, and resetting upon collision.
 """
 
 from turtle import Turtle
 
-# Constants for snake initialization and movement.
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]  # Initial positions for the snake's segments.
-MOVE_DISTANCE = 20  # Distance each segment moves per step.
+# Configuration constants.
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]  # Initial segment locations.
+MOVE_DISTANCE = 20  # Distance the head moves each step.
 
-# Direction constants in degrees.
+# Heading angles.
 RIGHT = 0
 UP = 90
 LEFT = 180
 DOWN = 270
 
-
 class Snake:
     """
-    Represents the snake in the game.
-
-    The snake is composed of multiple turtle segments that follow each other to create a moving snake.
+    Represents the player-controlled snake. Handles its segments, movement,
+    growth, and resetting when collisions occur.
     """
-
     def __init__(self):
-        self.snake_body = []  # List that holds all segments of the snake.
-        self.create_snake()  # Initialize the snake with default segments.
-        self.head = self.snake_body[0]  # The head of the snake for movement and collision detection.
+        self.snake_body = []        # List to hold the turtle segments.
+        self.create_snake()         # Populate initial segments.
+        self.head = self.snake_body[0]  # Reference to the first segment as the head.
 
     def create_snake(self):
         """
-        Creates the initial snake by adding segments at predefined starting positions.
+        Build the initial snake by placing segments at starting positions.
         """
         for position in STARTING_POSITIONS:
             self.add_body_part(position)
 
     def add_body_part(self, position):
         """
-        Adds a new segment to the snake at the given position.
+        Create a new square segment at the given position and add it to the body list.
 
         Args:
-            position (tuple): The (x, y) coordinates where the new segment will be placed.
+            position (tuple): (x, y) coordinates for the new segment.
         """
-        snake_body_part = Turtle(shape="square")
-        snake_body_part.color("white")
-        snake_body_part.penup()
-        snake_body_part.goto(position)
-        self.snake_body.append(snake_body_part)
+        segment = Turtle(shape="square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.snake_body.append(segment)
 
     def extend(self):
         """
-        Extends the snake by adding a new segment to its tail.
+        Add a new segment to the end of the snake at the last segment’s position.
         """
-        # Add a segment at the position of the last segment.
         self.add_body_part(self.snake_body[-1].position())
 
     def move(self):
         """
-        Moves the snake forward by moving each segment to the position of the segment ahead of it.
-        The head then moves forward by a fixed distance.
+        Update all segments to follow the one in front, then move the head forward.
         """
-        # Move each segment from tail to head.
-        for body_part in range(len(self.snake_body) - 1, 0, -1):
-            new_x = self.snake_body[body_part - 1].xcor()
-            new_y = self.snake_body[body_part - 1].ycor()
-            self.snake_body[body_part].goto(new_x, new_y)
-        # Move the head forward.
+        # Move each body segment to the position of the one ahead of it.
+        for idx in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[idx - 1].xcor()
+            new_y = self.snake_body[idx - 1].ycor()
+            self.snake_body[idx].goto(new_x, new_y)
+        # Advance the head segment.
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         """
-        Changes the snake's direction to up if it's not currently moving down.
+        Turn the snake’s head north unless it’s currently moving south.
         """
         if self.head.heading() != DOWN:
-            self.snake_body[0].setheading(UP)
+            self.head.setheading(UP)
 
     def down(self):
         """
-        Changes the snake's direction to down if it's not currently moving up.
+        Turn the snake’s head south unless it’s currently moving north.
         """
         if self.head.heading() != UP:
-            self.snake_body[0].setheading(DOWN)
+            self.head.setheading(DOWN)
 
     def left(self):
         """
-        Changes the snake's direction to left if it's not currently moving right.
+        Turn the snake’s head west unless it’s currently moving east.
         """
         if self.head.heading() != RIGHT:
-            self.snake_body[0].setheading(LEFT)
+            self.head.setheading(LEFT)
 
     def right(self):
         """
-        Changes the snake's direction to right if it's not currently moving left.
+        Turn the snake’s head east unless it’s currently moving west.
         """
         if self.head.heading() != LEFT:
-            self.snake_body[0].setheading(RIGHT)
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        """
+        Remove all existing segments off-screen, clear the body list,
+        and recreate the snake in its starting configuration.
+        """
+        for segment in self.snake_body:
+            segment.goto(1000, 1000)  # Move off-screen
+        self.snake_body.clear()       # Clear segment list
+        self.create_snake()           # Rebuild initial snake
+        self.head = self.snake_body[0]  # Reset head reference
