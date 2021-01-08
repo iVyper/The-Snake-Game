fc3c4ee70895c38cbf30abc79141f98416aa61ff
Ivis Perdomo <iperdomo@ivyper.dev>
2021-01-08 00:00:00 -0500

    refactor(main): reset game state instead of exiting on collision
    - Replace end-of-game flag with calls to scoreboard.reset() and snake.reset()
      on wall or self-collision.
    - Remove game termination logic to allow continuous play until user closes window.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 Main Module for The Snake Game
 
 This module ties together the game components: Snake, Food, and Scoreboard.
-It sets up the game screen, listens for user input, and runs the game loop until a collision occurs.
+It sets up the game screen, listens for user input, and runs the game loop.
+On collisions (with wall or self), the game resets instead of ending.
 """
 
 from turtle import Screen
@@ -16,14 +17,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game")
-screen.tracer(0)  # Turn off automatic animation to control screen updates manually.
+screen.tracer(0)  # Disable automatic screen updates for manual control.
 
 # Create game components.
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+snake = Snake()         # The moving snake
+food = Food()           # The food pellet
+scoreboard = Scoreboard()  # Score display and high score tracking
 
-# Set up keyboard bindings.
+# Configure keyboard controls for snake direction.
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -33,28 +34,31 @@ screen.onkey(snake.right, "Right")
 # Main game loop.
 game = True
 while game:
-    screen.update()     # Refresh the screen.
-    time.sleep(0.1)     # Pause the loop to control game speed.
+    screen.update()     # Manually refresh the screen.
+    time.sleep(0.1)     # Control the game speed.
 
-    snake.move()        # Move the snake forward.
+    snake.move()        # Advance the snake forward.
 
-    # Check for collision with the food.
+    # --- Food collision check ---
+    # If the snake head is close enough to the food, "eat" it:
     if snake.head.distance(food) < 15:
-        food.refresh()         # Move food to a new random location.
-        snake.extend()         # Add a new segment to the snake.
-        scoreboard.increase_score()  # Update the score.
+        food.refresh()         # Reposition the food randomly.
+        snake.extend()         # Grow the snake by one segment.
+        scoreboard.increase_score()  # Increment and display score.
 
-    # Check for collision with the wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or \
-       snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game = False
-        scoreboard.game_over()
+    # --- Wall collision check ---
+    # If the head crosses the boundary, reset the game state:
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
+        scoreboard.reset()     # Update high score if needed and reset display.
+        snake.reset()          # Remove all segments and recreate the snake at start.
 
-    # Check for collision with the snake's body.
-    for body_part in snake.snake_body[1:]:
-        if snake.head.distance(body_part) < 10:
-            game = False
-            scoreboard.game_over()
+    # --- Self-collision check ---
+    # If the head runs into any segment of its body, reset:
+    for segment in snake.snake_body[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+            break  # Exit the loop once reset has occurred
 
-# Keep the game window open until clicked.
+# Keep the window open until the user closes it.
 screen.exitonclick()
